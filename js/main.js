93c62ec88252c845b48bfbc96bfe9829cdd95c23
Nina <ninahovinmaa@gmail.com>
2020-10-07 15:01:42 +0200

    finished buildQuiz section, finetunes

--- js/main.js
+++ js/main.js
@@ -1,36 +1,34 @@
 
 //------Functions------------//
 
-function buildQuiz() { 
-                                                                             // will run immediately
+function buildQuiz() {     // will run immediately
+
     const output = [];              // variable to store the HTML output
     
     myQuestions.forEach((currentQuestion, questionNumber) => {           //for each question...The forEach() method calls a function once for each element in an array, in order.
     
        const answers =[];                                //variable to store the list of possible answers
-
-       
        
         for(letter in currentQuestion.answers){          // and for each available answer...The for/in statement loops through the properties of an object.
                                                         // ...add an HTML checkbox/radiobutton
             answers.push(
-                '<label>'+
-                '<input type="checkbox" name="question"'+ questionNumber + 'value=' + letter + letter + ':' + currentQuestion
-                .answers[letter]+'</label>'
+
+                '<label><input type="checkbox" name="question'+ questionNumber +'"'+ ' value="'+ letter +'"><br>'+ letter + ':' + currentQuestion.answers[letter]+'</label>'
             )
         }
         
         output.push(                                  // add this question and its answers to the output
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+        '<div class="question">' + currentQuestion.question + '<div>' + '<div class="answers">' + answers.join()+'</div>'
         );
      });         
     
    quizContainer.innerHTML = output.join('');                                             // finally combine our output list into one string of HTML and put it on the page
 }
 
-
-function showResults() {}               // runs when user clicks submit
+ // runs when user clicks submit
+function showResults() {
+    //const answerContainers = quizContainer.querySelectorAll('.answers');
+}              
 
 
 
@@ -51,16 +49,12 @@ document.addEventListener("DOMContentLoaded", function(){
         await fetch('https://quizapi.io/api/v1/questions?apiKey=DzqPPQrygBSBX2YJQ10caUO91MtOWmACoFhEsKYg&limit=5')
             .then(response => response.json())
             .then(data => {myQuestions = Array.from(data)});  
-    
-            buildQuiz(); //display quiz
         
-    
-    });
-
-    
+            buildQuiz(); //display quiz       
+    });    
 });
                      
-//on submit, show results
-submitButton.addEventListener("click", showResults);
+
+
 
 
