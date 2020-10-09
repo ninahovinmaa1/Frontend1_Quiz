61302c0e45ce54579098f66b921402ffe8943799
Nina <ninahovinmaa@gmail.com>
2020-10-09 21:14:06 +0200

    trying to solve issue with multiple answers but no success so far

--- js/main.js
+++ js/main.js
@@ -22,27 +22,28 @@ function buildQuiz() {     // will run immediately
         );
      });         
     
-   quizContainer.innerHTML = output.join('');  // finally combine our output list into one string of HTML and put it on the page
+   quizContainer.innerHTML = output.join('');  // Combine utput list into one string of HTML and put it on the page
 }
 
  // runs when user clicks submit
 function showResults() {
 
     const answerContainers = quizContainer.querySelectorAll('.answers');
-
+    
     // counter of user's answers
     let numCorrect = 0;
 
     // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
+    myQuestions.forEach((currentQuestion, questionNumber) => {
         // find selected answer
         const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`;
         
-        const userAnswers = answerContainer.querySelectorAll(selector);  // not working. trying to get multiple correct answers
-        for (let checkbox of userAnswers) {
-            checkbox.checked = true;
-}           console.log(checkbox);
+        const userAnswers = (answerContainer.querySelector(selector) || {}).value;  //  Trying to get multiple correct answers with querySelectorAll->Nodelist-array, undefined. Check use of .value for arrayen. TODO
+        console.log(userAnswers) //eget labb
+        // for (let checkbox of userAnswers) {
+        //    checkbox.checked = true;
+       //    console.log(checkbox);
         //const userAnswer = (answerContainer.querySelectorAll(selector) ||{}); //answers or not answered
         
         
