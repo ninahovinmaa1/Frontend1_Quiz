853f33d0cc74fba4f67ee7c418cd97dd55ccb643
Nina <ninahovinmaa@gmail.com>
2020-10-12 12:38:44 +0200

    cleanup of file startquiz

--- js/startquiz.js
+++ js/startquiz.js
@@ -1,12 +1,3 @@
-/*
-class Player {
-    constructor (name) {
-        this.name = name;
-        this.quizScores = [];
-        this.currentScore = 0;
-    };
-};
-*/
 
 class Player {
   constructor(name) {
